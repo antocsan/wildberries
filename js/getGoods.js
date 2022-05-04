89f42aaa749cd8589beb8afca7cd488a1fcee92f
antocsan <93600250+antocsan@users.noreply.github.com>
2022-05-04 10:48:39 +0300

    feat:cart.js search.js getGoods.js

---
+++ js/getGoods.js
@@ -0,0 +1,28 @@
+const getGoods = () => {
+
+    const links = document.querySelectorAll('.navigation-link')
+
+
+
+    const getData = (value, category) => {
+        fetch('db/db.json')
+            .then((res) => res.json())
+            .then((data) => {
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array));
+            })
+    }
+    links.forEach((link) => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+            getData(linkValue, category)
+        })
+
+    })
+
+}
+
+getGoods()
